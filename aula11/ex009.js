b0d49931d78f85f8434024cc9228ac8e2b2e7af8
GabrielMTF <gabrielmota287@gmail.com>
2021-02-14 12:17:03 -0300

    curso de js

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var vel = 90
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 80) { //CONDIÇÃO SIMPLES
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+} else {
+    console.log('Dirija sempre com segurança!')
+}
+console.log('Dirija sempre usando cinto de segurança')
